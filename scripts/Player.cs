66a2ddc1eadea5336d172278a09fdac7b21d44f2
rizwaradnan03 <rizwar.adnanbyantara@gmail.com>
2025-09-06 11:37:05 +0700

    add movement for player

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,8 +3,55 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	float playerSpeed = 100;
+	bool isMoving = false;
 
-    public override void _Process(double delta)
-    {
-    }
+	public void Move(float delta)
+	{
+		if (Input.IsActionPressed("move_top") || Input.IsActionPressed("move_bottom") || Input.IsActionPressed("move_right") || Input.IsActionPressed("move_left"))
+		{
+			isMoving = true;
+			float posX = Position.X;
+			float posY = Position.Y;
+
+			if (Input.IsActionPressed("move_top") || Input.IsActionPressed("move_bottom"))
+			{
+				if (Input.IsActionPressed("move_top"))
+				{
+					posY = Position.Y - (delta * playerSpeed);
+				}
+				else
+				{
+					posY = Position.Y + (delta * playerSpeed);
+				}
+			}
+			else if (Input.IsActionPressed("move_left") || Input.IsActionPressed("move_right"))
+			{
+				if (Input.IsActionPressed("move_left"))
+				{
+					posX = Position.X - (delta * playerSpeed);
+				}
+				else
+				{
+					posX = Position.X + (delta * playerSpeed);
+				}
+			}
+
+			Position = new Vector2(posX, posY);
+		}
+		else
+		{
+			isMoving = false;
+		}
+
+		if (isMoving == true)
+		{
+			MoveAndSlide();
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		Move((float)delta);
+	}
 }
